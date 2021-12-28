4862132cf5835f76c58bc6da60c977af430110b8
Lucas Mendes <lucas.gabriel3.lg@gmail.com>
2021-12-28 15:24:55 -0300

    Lab 2

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Instructions
+
+You were assigned a project to create a script that will help a school with a wacky database to organize their data, claculate their end of year gradings and inform students and parents.
+
+You will use:
+
+- Lists
+- Basic Math Oprations
+- Attributes
+- Functions
+- Conditionals
+
+# How to do the Lab?
+
+- 1: Fork this repository to your GitHub
+- 2: Clone this repository locally using git clone
+- 3: Work on the code
+- 4: Once finished, don't forget to git add and git commit the project
+- 5: Push the repository to GitHub
+- 6: Create a Pull Request to me once the project is finished!
+
+And of course, ask me if you need any help! =)
---
+++ students_grades.py
@@ -0,0 +1,52 @@
+# You should create a script that will help this school organizing their students and their respective grades;
+# Here is the list of students and their grades:
+
+class_name = "bellsworth wiston school - second grade b"
+
+students = ["dave braninghan", "susan goodwin",
+            "adam lowel", "linda lydon", "jessie boham"]
+
+# Below you can find lists with their grades for each trimester
+
+dave_braninghan_grades = [10.0, 9.2, 8.5, 7.0]
+susan_goodwin_grades = [7.0, 8.5, 6.0, 10.0]
+adam_lowel_grades = [6.0, 6.0, 8.5, 5.0]
+linda_lydon_grades = [9.5, 10.0, 10.0, 5.5]
+jessie_boham_grades = [10.0, 3.5, 7.75, 9.0]
+
+# ITERATION 1
+# As you can see, the student names are not in order. Look for a Python attribute you can use to sort the students names in alphabetical order.
+# Use a print once you do that just to make sure that the names were correctly ordered.
+# Once that is done and verified, you can comment out the print
+# Your code goes below this line
+
+
+# ITERATION 2
+# We currently have all the grades for each student. Calculate the avarage grade for each student and store the result in a variable.
+# Your code goes below this line
+
+
+# ITERATION 3
+# Print a message for each student saying "Hellow *Student Name*. Your final grade this year is *Avarage Grade*"
+# As you can see, the student's names are not capitalized and that's just rude. Make sure when you print them, their first letters are capitalized.
+# Your code goes below this line
+
+
+# ITERATION 4
+# Now, store all the student's avarage grades inside a new list that we will use to calculate the entire class avarage.
+# TIP: you can store variables in a list.
+# Once you have a list with all students avarage grades, calculate the entire class avarage.
+# Your code goes below this line
+
+
+# ITERATION 5
+# Calculate how many students are in this class using a Python function and store it on a variable
+# Print a message stating what was the avarage class grade and how many students were in the class.
+# The message should have the class name and it should be ALL IN CAPITAL LETTERS.
+
+
+# BONUS ITERATION
+# Now, if you are feeling courageous, we can go a step ahead and create a function with a conditional
+# If the student had an average grade higher than 7, print a message that have made it to the next grade.
+# If their grade is lower than 7, they have not made it and will have to go through second grade again.
+# I can help you on this one! =)
